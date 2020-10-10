6c74828853ac8acdce80d8a6a189415d6ff4d150
Natanael <natanael@timipro.com.br>
2020-10-09 21:42:25 -0300

    Criar interface de filtros

--- app/src/main/java/br/natanael/android/instagram/activity/FiltroActivity.java
+++ app/src/main/java/br/natanael/android/instagram/activity/FiltroActivity.java
@@ -1,10 +1,15 @@
 package br.natanael.android.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import br.natanael.android.instagram.R;
@@ -22,6 +27,8 @@ public class FiltroActivity extends AppCompatActivity {
         //Inicializar componentes
         imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
 
+        configurarToolbar();
+
         //Recupera a imagem escolhida pelo Usuario
         Bundle bundle = getIntent().getExtras();
 
@@ -35,9 +42,43 @@ public class FiltroActivity extends AppCompatActivity {
                 imageFotoEscolhida.setImageBitmap(imagem);
             }
         }
+    }
 
+    private void configurarToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Filtros");
+        setSupportActionBar(toolbar);
 
+        //Habilitar botao voltar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //Alterar o icone do botao voltar
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_filtro, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.ic_salvar_postagem:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
