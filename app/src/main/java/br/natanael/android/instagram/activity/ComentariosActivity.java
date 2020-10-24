4ad80953717de0ba4483d982ee11daac8bcecf20
Natanael <natanael@timipro.com.br>
2020-10-23 21:45:12 -0300

    Salvar Comentarios

--- app/src/main/java/br/natanael/android/instagram/activity/ComentariosActivity.java
+++ app/src/main/java/br/natanael/android/instagram/activity/ComentariosActivity.java
@@ -4,18 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.natanael.android.instagram.R;
+import br.natanael.android.instagram.helper.UsuarioFirebase;
+import br.natanael.android.instagram.model.Comentario;
+import br.natanael.android.instagram.model.Usuario;
 
 public class ComentariosActivity extends AppCompatActivity {
+
+    private EditText editComentario;
+    private String idPostagem;
+    private Usuario usuario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comentarios);
 
+        //Inicializa componentes
+        editComentario = findViewById(R.id.editComentario);
+
+        //configuracoes iniciais
+        usuario = UsuarioFirebase.getDadosUsuarioLogado();
+
+
         configurarToolbar();
 
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null)
+        {
+            idPostagem = bundle.getString("idPostagem");
+        }
     }
 
     private void configurarToolbar() {
@@ -28,5 +51,49 @@ public class ComentariosActivity extends AppCompatActivity {
 
         //Alterar o icone do botao voltar
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
+    }
+
+    public void salvarComentario(View view) {
+        String textoComentario = editComentario.getText().toString();
+
+        if(textoComentario != null)
+        {
+            if(!textoComentario.equals(""))
+            {
+                Comentario comentario = new Comentario();
+                comentario.setIdPostagem(idPostagem);
+                comentario.setIdUsuario(usuario.getId());
+                comentario.setNomeUsuario(usuario.getNome());
+                comentario.setCaminhoFoto(usuario.getCaminhoFoto());
+                comentario.setComentario(textoComentario);
+
+                if(comentario.salvar())
+                {
+                    Toast.makeText(this, "Comentário salvo com sucesso", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(this, "Erro ao salvar", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+            else
+                Toast.makeText(this, "Digite um comentário", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Digite um comentário", Toast.LENGTH_SHORT).show();
+        }
+
+        editComentario.setText("");
+
     }
 }
---
+++ app/src/main/java/br/natanael/android/instagram/model/Comentario.java
@@ -0,0 +1,79 @@
+package br.natanael.android.instagram.model;
+
+import com.google.firebase.database.DatabaseReference;
+
+import br.natanael.android.instagram.helper.ConfiguracaoFirebase;
+
+public class Comentario {
+    private String idComentario;
+    private String idPostagem;
+    private String idUsuario;
+    private String caminhoFoto;
+    private String nomeUsuario;
+    private String comentario;
+
+    public Comentario() {
+
+    }
+
+    public String getIdComentario() {
+        return idComentario;
+    }
+
+    public void setIdComentario(String idComentario) {
+        this.idComentario = idComentario;
+    }
+
+    public String getIdPostagem() {
+        return idPostagem;
+    }
+
+    public void setIdPostagem(String idPostagem) {
+        this.idPostagem = idPostagem;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getCaminhoFoto() {
+        return caminhoFoto;
+    }
+
+    public void setCaminhoFoto(String caminhoFoto) {
+        this.caminhoFoto = caminhoFoto;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public boolean salvar() {
+        DatabaseReference comentariosRef = ConfiguracaoFirebase.getFirebase()
+                .child("comentarios")
+                .child(getIdPostagem());
+
+        String chaveComentario = comentariosRef.push().getKey();
+
+        setIdComentario(chaveComentario);
+        comentariosRef.child(getIdComentario()).setValue(this);
+
+        return true;
+    }
+}
